b989b545e3a055ca3a10feb4ac058df1e5688164
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-04-13 21:21:42 +0530

    Adding Refind feed

--- scripts/router/index.js
+++ scripts/router/index.js
@@ -5,13 +5,15 @@ Vue.use(Router)
 
 import RedditView from '../views/RedditView.vue'
 import MediumView from '../views/MediumView.vue'
+import RefindView from '../views/RefindView.vue'
 import BlogFeedView from '../views/BlogFeedView.vue'
 
 export default new Router({
   mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: [
-    { path: '/reddit', component: RedditView },
+    { path: '/reddit', component: RedditView },    
+    { path: '/refind', component: RefindView },
     { path: '/medium', component: MediumView },
     { path: '/:blogID', component: BlogFeedView },
     { path: '*', redirect: '/reddit' }
---
+++ scripts/views/RefindView.vue
@@ -0,0 +1,31 @@
+<template>
+  <news-list :posts="posts"></news-list>
+</template>
+
+<script>
+import NewsList from './../components/NewsList.vue'
+import {fetchRefindPosts} from './../apis/refind-api'
+export default {
+  name: 'refind-view',
+  components: {
+    NewsList
+  },
+  data () {
+      return {
+          posts: []
+      }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData () {
+      this.posts = []
+      fetchRefindPosts(this.loadPosts)
+    },
+    loadPosts (result) {
+      this.posts = result
+    }
+  }
+}
+</script>
